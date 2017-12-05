2369787e97c3500318cc16d2c7c80cbe3314e4dd
LinarAbzaltdinov <abzaltdinov@gmail.com>
2017-12-05 17:56:14 +0300

    Added Twig to project, moved logic and views to diff folders

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-//session_start();
- /*
-if (isset($_SESSION['username'])) {
-    header("location: views/balance.php");
-} else {
-    header("location: views/login.php");
-}*/
- echo DB::instance()->query('Select * from Users')->columnCount();
--- views/balance.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: polzovatel
- * Date: 02/12/2017
- * Time: 02:39
- */
