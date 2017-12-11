9afaffc53abd7bc7726b5626a0bfe6e28633b2ad
LinarAbzaltdinov <ner-1995>
2017-12-12 00:47:58 +0300

    bug fixes (exchange rates)

--- src/classes/ExchangeRates.php
+++ src/classes/ExchangeRates.php
@@ -12,17 +12,25 @@ class ExchangeRates
     static private $rates;
     static public function getRatesFromApi() //all exchange rates to RUB
     {
+        require_once "DB.php";
         $today = date("Y-m-d");
         $lastUpdateWeekday = date('w', self::$dateOfLastUpdate);
         if (!isset(self::$dateOfLastUpdate) ||
             $today != self::$dateOfLastUpdate &&
             !($lastUpdateWeekday == 5 && date_diff(self::$dateOfLastUpdate, $today) <= 2)) // don't update at weekend
         {
+            $db = DB::instance();
+            $res = $db->query("SELECT name FROM Currency")->fetchAll(PDO::FETCH_ASSOC);
+            $dbCurrencies = array();
+            foreach ($res as $row) {
+                $dbCurrencies[$row['name']] = true;
+            }
             $json = file_get_contents("http://api.fixer.io/$today?base=RUB");
             $obj = json_decode($json, true);
             self::$dateOfLastUpdate = $obj['date'];//date('Y-m-d',);
-            $func = function ($val) { return 1/$val; };
-            self::$rates = array_map($func, obj['rates']);
+            $reverse = function ($val) { return number_format(1.0/$val, 2, ".", ""); };
+            $tr = array_intersect_key($obj["rates"], $dbCurrencies);
+            self::$rates = array_map($reverse, $tr);
         }
         return self::$rates;
     }
--- web/lk.php
+++ web/lk.php
@@ -1,5 +1,7 @@
 <?php
 session_start();
+if (!isset($_SESSION['username']))
+    header("location: index.php");
 require_once __DIR__.'/../src/classes/Account.php';
 require_once __DIR__.'/../src/classes/DB.php';
 require_once __DIR__.'/../src/DBconfig.php';
